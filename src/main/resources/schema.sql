7bd09268869d49d2feba6080239ae578ebbcca7d
kamzob <149818908+kamzob@users.noreply.github.com>
2026-05-09 20:59:37 +0300

    Added wishlist to db schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,17 +1,25 @@
 CREATE TABLE room (
                       id SERIAL PRIMARY KEY,
-                      code VARCHAR(10) UNIQUE
+                      code VARCHAR(10) UNIQUE NOT NULL,
+                      status VARCHAR(20) NOT NULL
 );
 
 CREATE TABLE participant (
                              id SERIAL PRIMARY KEY,
-                             name VARCHAR(100),
+                             public_id UUID UNIQUE NOT NULL,
+                             name VARCHAR(100) NOT NULL,
                              room_id INT REFERENCES room(id)
+);
+
+CREATE TABLE wishlist_item (
+                               id SERIAL PRIMARY KEY,
+                               participant_id INT REFERENCES participant(id),
+                               item_text VARCHAR(300) NOT NULL
 );
 
 CREATE TABLE pair (
                       id SERIAL PRIMARY KEY,
-                      giver VARCHAR(100),
-                      receiver VARCHAR(100),
+                      giver_participant_id INT REFERENCES participant(id),
+                      receiver_participant_id INT REFERENCES participant(id),
                       room_id INT REFERENCES room(id)
 );
